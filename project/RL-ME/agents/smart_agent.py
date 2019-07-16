21bafdad73aaae1826dc0b273075d6fc9d5bb0f0
Aqeel <aqeel@gmail.com>
2019-07-17 01:22:36 +0300

    add QC project

---
+++ project/RL-ME/agents/smart_agent.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+import random
+from collections import namedtuple, deque
+
+class ReplayBuffer:
+    """Fixed-size buffer to store experience tuples."""
+
+    def __init__(self, buffer_size, batch_size):
+        """Initialize a ReplayBuffer object.
+        Params
+        ======
+            buffer_size: maximum size of buffer
+            batch_size: size of each training batch
+        """
+        self.memory = deque(maxlen=buffer_size)  # internal memory (deque)
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+
+    def add(self, state, action, reward, next_state, done):
+        """Add a new experience to memory."""
+        e = self.experience(state, action, reward, next_state, done)
+        self.memory.append(e)
+
+    def sample(self, batch_size=64):
+        """Randomly sample a batch of experiences from memory."""
+        return random.sample(self.memory, k=self.batch_size)
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.memory)
+
+
+class OUNoise:
+    """Ornstein-Uhlenbeck process."""
+
+    def __init__(self, size, mu, theta, sigma):
+        """Initialize parameters and noise process."""
+        self.mu = mu * np.ones(size)
+        self.theta = theta
+        self.sigma = sigma
+        self.reset()
+
+    def reset(self):
+        """Reset the internal state (= noise) to mean (mu)."""
+        self.state = self.mu
+
+    def sample(self):
+        """Update internal state and return it as a noise sample."""
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(len(x))
+        self.state = x + dx
+        return self.state
+
+class Actor:
+    def
+    __init__(self,in_size,out_size,act_min,act_max,lr=0.001,nl=3,activation=['relu','relu','sigmoid'],num_nodes
+            =[400,300]):
+        self.in_size = in_size
+        self.out_size = out_size
+        self.act_min = act_min
+        self.act_max = act_max
+        self.lr = lr
+    def Create_network(self):
+        x = Input(self.in_size)
+        for i in range(self.nl):
+
+        pass
+    pass
+class Critic:
+    pass
+class DDPG:
+    pass
